589fe6f6331db293cffb84d203ca954a47907ce5
Grant Wilson <ghvvilson@gmail.com>
2024-07-16 09:22:06 +0200

    feat

--- publish-workstation-configuration.sh
+++ publish-workstation-configuration.sh
@@ -1,4 +1,14 @@
-PROJECT_ID=grant-wilson
+declare -A gcp_projects
+gcp_projects["adg"]="adg-internal-workstations"
+
+if [ "$#" -ne 1 ]; then
+    echo "Usage: $0 <project_key>"
+    echo "Available projects: ${!gcp_projects[@]}"
+    exit 1
+fi
+
+PROJECT_ID=${gcp_projects[$1]:-$1}
+echo "Selected project: $PROJECT_ID"
 gcloud services enable artifactregistry.googleapis.com cloudbuild.googleapis.com --project $PROJECT_ID
 
 gcloud artifacts repositories create default --repository-format=docker \
